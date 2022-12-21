abe92998bd597d0a4a449814ee4fc4e0cbca4cd8
Naimish <naimishsakhavala125@gmail.com>
2022-12-21 10:02:39 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Email.dart';
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      )
+  );
+}
